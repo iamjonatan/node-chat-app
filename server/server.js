77ca5dfdce72073575c56d650a3d85845782866d
Jonathan Chirwa <chirwaj@delmar-group.com>
2018-07-06 09:27:57 -0400

    server - client connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 // JavaScript source code
 //https://floating-wildwood-61985.herokuapp.com/
+
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);             //returns web socket server, used to emit or listen to events
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log("client diconnected!");
+    });
+});      //listener
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
